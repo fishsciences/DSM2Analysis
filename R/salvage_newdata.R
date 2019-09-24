5f885ea22923c4d06e14a83011e8cdfd1785f03b
Travis Hinkelman <travis.hinkelman@gmail.com>
2019-09-23 21:14:21 -0700

    Fix salvage_newdata

--- R/salvage_newdata.R
+++ R/salvage_newdata.R
@@ -4,17 +4,16 @@
 #'
 #' @md
 #' @param run              Chinook Salmon run: Spring, Fall, LateFall, Winter
-#' @param facility         Water export facility: CVP, SWP, both
 #' @param flow_df          Data frame with modeled flow input
 #' @param annual_passage   Arbitrary annual passage used salvage predictions
 #'
 #' @export
 #'
 
-salvage_newdata <- function(run, facility, flow_df, annual_passage = 1e6){
+salvage_newdata <- function(run, flow_df, annual_passage = 1e6){
   ss <- sac_salvage[sac_salvage[[run]] == 1,]
   nd <- flow_df %>%
-    left_join(ws) %>%
+    left_join(winter_run_size) %>%
     mutate(sac_in_z = z_score(Freeport, mean(ss$sac_in, na.rm = TRUE), sd(ss$sac_in, na.rm = TRUE)),
            CWP_z = z_score(CVP, mean(ss$CVP, na.rm = TRUE), sd(ss$CVP, na.rm = TRUE)),
            SWP_z = z_score(SWP, mean(ss$SWP, na.rm = TRUE), sd(ss$SWP, na.rm = TRUE)),
