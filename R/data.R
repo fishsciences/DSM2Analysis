448d161afb4cf4692f3bbb788d678df50363e0d7
Travis Hinkelman <travis.hinkelman@gmail.com>
2020-02-21 15:42:53 -0800

    Restructure data documentation

--- R/data.R
+++ R/data.R
@@ -1,3 +1,11 @@
+#' Sacramento River salvage
+#'
+#' A dataset on number of fish salvaged at CVP and SWP based on releases of tagged fish in the North Delta.
+#'
+#' @format A tibble with 749 rows and 32 columns
+#'
+"sac_salvage"
+
 #' Winter run size
 #'
 #' Estimates of juvenile winter run fork lengths by month; used in salvage predictions.
--- R/sac_salvage.R
+++
@@ -1,7 +0,0 @@
-#' Sacramento River salvage
-#'
-#' A dataset on number of fish salvaged at CVP and SWP based on releases of tagged fish in the North Delta.
-#'
-#' @format A tibble with 749 rows and 32 columns
-#'
-"sac_salvage"
