ebbe09bb57f504b6e1acb8f2939d5491b1abae4a
Myfanwy <myfanwy@github.com>
2021-05-12 13:12:59 -0700

    remove EM arg from salvage_model

--- R/salvage_model.R
+++ R/salvage_model.R
@@ -12,11 +12,15 @@
 salvage_model <- function(facility){
   ss <- sac_salvage[sac_salvage$winter == 1,] # fit model based on winter-run observations
   if(facility == "SWP"){
-    sm <- pscl::zeroinfl(sal_swp ~ sac_in_z + SWP_z + length_z + offset(log_released)| sac_in_z + SWP_z + length_z, data = ss, dist = "negbin", EM = TRUE)
+    sm <- pscl::zeroinfl(sal_swp ~ sac_in_z + SWP_z + length_z + offset(log_released)| sac_in_z + SWP_z + length_z, data = ss, dist = "negbin")
+
   } else if(facility == "CVP"){
-    sm <- pscl::zeroinfl(sal_cvp ~ sac_in_z + CVP_z + length_z + offset(log_released)| sac_in_z + CVP_z + length_z, data = ss, dist = "negbin", EM = TRUE)
+
+    sm <- pscl::zeroinfl(sal_cvp ~ sac_in_z + CVP_z + length_z + offset(log_released)| sac_in_z + CVP_z + length_z, data = ss, dist = "negbin")
+
   } else {
-    sm <- pscl::zeroinfl(sal_tot ~ sac_in_z + exports_z + length_z + offset(log_released)| sac_in_z + exports_z + length_z, data = ss, dist = "negbin", EM = TRUE)
+    sm <- pscl::zeroinfl(sal_tot ~ sac_in_z + exports_z + length_z + offset(log_released)| sac_in_z + exports_z + length_z, data = ss, dist = "negbin")
+
   }
   return(sm)
 }
